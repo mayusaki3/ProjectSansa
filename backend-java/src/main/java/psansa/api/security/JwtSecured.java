f9ffe0b88c7facb58374be5b9201a4c35d388ea9
m_okada <m_okada@system-tokai.co.jp>
2025-09-19 18:02:35 +0900

    再構築

---
+++ backend-java/src/main/java/psansa/api/security/JwtSecured.java
@@ -0,0 +1,9 @@
+package psansa.api.security;
+
+import jakarta.ws.rs.NameBinding;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@NameBinding
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JwtSecured {}
