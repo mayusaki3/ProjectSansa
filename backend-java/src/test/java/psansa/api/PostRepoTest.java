f5b0dfa2942c678575e1098f330f95468f12f23e
mayusaki3 <masayuki@fa2.so-net.ne.jp>
2025-09-20 12:05:35 +0900

    再構築

---
+++ backend-java/src/test/java/psansa/api/PostRepoTest.java
@@ -0,0 +1,12 @@
+// src/test/java/psansa/api/PostRepoTest.java
+package psansa.api;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostRepoTest {
+    @Test
+    void example() {
+        assertTrue(true);
+    }
+}
